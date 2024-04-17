ce647846f02951049df712bb49c738d54d4319a4
LZal <lzaleski001@student.franciscan.edu>
2024-04-17 19:00:08 -0400

    done

--- src/m1_movie_theater.py
+++ src/m1_movie_theater.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# TODO: 1. (3 pts)
+# DONE: 1. (3 pts)
 #
 #   For this module, we are going to create a basic program to handle movie
 #   times at a movie theater. For our purposes, a movie is going to be defined
@@ -28,9 +28,19 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+ 
+def show_movies(movies):
+    print("Movies:")
+    for x in movies:
+        print()
+        print(f"Title: " + x["title"])
+        print("Duration: " + x["duration"])
+        print("Start Time: " + x["start_time"])
+        print("Theater: " + x["theater_num"])
+        print("Tickets Available: " + str(x["num_of_tickets"]))
 
 ###############################################################################
-# TODO: 2. (3 pts)
+# DONE: 2. (3 pts)
 #
 #   For this _todo_, write a function called get_ticket() that takes one
 #   parameter:
@@ -52,8 +62,18 @@
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
 
+def get_ticket(movie):
+    if int(movie["num_of_tickets"]) > 0:
+        movie["num_of_tickets"] = int(movie["num_of_tickets"]) - 1
+        print("Success! You now have a ticket for " + movie["title"] + "!")
+    else:
+        print("There are currently no tickets available for that movie.")
+
+
+
+
 ###############################################################################
-# TODO: 3. (9 pts)
+# DONE: 3. (9 pts)
 #
 #   Now, let's create our movie showtimes system.
 #
@@ -83,3 +103,40 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+
+
+def main():
+    print("Welcome to the movie theater!")
+    movies = []
+
+    while True:
+        title = input("What is the movie's title? ")
+        if title == "end": break
+        duration = input("How many minutes long is the movie? ")
+        if duration == "end": break
+        start_time = input("What time does the movie start? ")
+        if start_time == "end": break
+        theater_num = input("What is the theater number for the moveie ")
+        if theater_num == "end": break
+        num_of_tickets = input("How many tickets are available? ")
+        if num_of_tickets == "end": break
+        movie = {
+            "title": title,
+            "duration": duration,
+            "start_time": start_time,
+            "theater_num": theater_num,
+            "num_of_tickets": num_of_tickets
+        }
+        movies.append(movie)
+        
+    show_movies(movies)
+    while True:
+        go = input("Which movie would you like to buy a ticket for? ")
+        if go == "end": break
+        for x in movies:
+            title == x["title"]
+            if go == x["title"]:
+                get_ticket(x)
+                show_movies(movies)
+
+main()
--- src/m3_atm.py
+++ src/m3_atm.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 ###############################################################################
-# TODO: 1. (2 pts)
+# DONE: 1. (2 pts)
 #
 #   The todos in this module are in one comment because you will be modifying
 #   the same bit of code each time. Here you will create a basic ATM
@@ -13,7 +13,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 2. (3 pts)
+# DONE: 2. (3 pts)
 #
 #   For this _todo_, you will create an area where the user's current balance
 #   is displayed. There should be a label that says "Current Balance ($):" and
@@ -27,7 +27,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 3 (3 pts)
+# DONE: 3 (3 pts)
 #
 #   For this _todo_, create two more labels: one that says "Amount ($):" and
 #   another that starts out empty beneath it. This is where the user's amount
@@ -36,7 +36,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 4. (7 pts)
+# DONE: 4. (7 pts)
 #
 #   For this _todo_, you will create all the buttons that the user needs:
 #
@@ -52,7 +52,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 5. (10 pts)
+# DONE: 5. (10 pts)
 #
 #   For this _todo_, using the command keyword on each button to have each
 #   number button type that digit in the amount label above (just like you
@@ -78,7 +78,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 5. (3 pts)
+# DONE: 5. (3 pts)
 #
 #   For this _todo_, bind the window to any keypress so that if the user types
 #   a number, it also types that number into the amount label. Remember, you
@@ -86,3 +86,102 @@ import tkinter as tk
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+
+window = tk.Tk()
+window.title("ATM")
+
+label1 = tk.Label(master = window, text = "Current Balance ($):")
+label1.pack()
+moneys = tk.Label(master = window, text = "1000")
+moneys.pack()
+
+label2 = tk.Label(master = window, text = "Amount ($):")
+label2.pack()
+change = tk.Label(master = window, text = "")
+change.pack()
+
+frame = tk.Frame(master = window)
+frame.rowconfigure([0,1,2,3], minsize = 75)
+frame.columnconfigure([0,1,2], minsize = 75)
+frame.pack()
+
+def b1():
+    change["text"] = change["text"] + "1"
+
+def b2():
+    change["text"] = change["text"] + "2"
+
+def b3():
+    change["text"] = change["text"] + "3"
+
+def b4():
+    change["text"] = change["text"] + "4"
+
+def b5():
+    change["text"] = change["text"] + "5"
+
+def b6():
+    change["text"] = change["text"] + "6"
+
+def b7():
+    change["text"] = change["text"] + "7"
+
+def b8():
+    change["text"] = change["text"] + "8"
+
+def b9():
+    change["text"] = change["text"] + "9"
+
+def b0():
+    change["text"] = change["text"] + "0"
+
+def withdrawal():
+    moneys["text"] = int(moneys["text"]) - int(change["text"])
+    change["text"] = ""
+
+def deposit():
+    moneys["text"] = int(moneys["text"]) + int(change["text"])
+    change["text"] = ""
+
+button1 = tk.Button(master = frame, text = "1", command = b1)
+button1.grid(row = 0, column = 0, sticky = "nsew")
+
+button2 = tk.Button(master = frame, text = "2", command = b2)
+button2.grid(row = 0, column = 1, sticky = "nsew")
+
+button3 = tk.Button(master = frame, text = "3", command = b3)
+button3.grid(row = 0, column = 2, sticky = "nsew")
+
+button4 = tk.Button(master = frame, text = "4", command = b4)
+button4.grid(row = 1, column = 0, sticky = "nsew")
+
+button5 = tk.Button(master = frame, text = "5", command = b5)
+button5.grid(row = 1, column = 1, sticky = "nsew")
+
+button6 = tk.Button(master = frame, text = "6", command = b6)
+button6.grid(row = 1, column = 2, sticky = "nsew")
+
+button7 = tk.Button(master = frame, text = "7", command = b7)
+button7.grid(row = 2, column = 0, sticky = "nsew")
+
+button8 = tk.Button(master = frame, text = "8", command = b8)
+button8.grid(row = 2, column = 1, sticky = "nsew")
+
+button9 = tk.Button(master = frame, text = "9", command = b9)
+button9.grid(row = 2, column = 2, sticky = "nsew")
+
+button0 = tk.Button(master = frame, text = "0", command = b0)
+button0.grid(row = 3, column = 1, sticky = "nsew")
+
+button_withdrawal = tk.Button(master = frame, text = "Withdrawal", command = withdrawal)
+button_withdrawal.grid(row = 3, column = 0, sticky = "nsew")
+
+button_deposit = tk.Button(master = frame, text = "Deposit", command = deposit)
+button_deposit.grid(row = 3, column = 2, sticky = "nsew")
+
+def keypress(event):
+    if event.char.isdigit():
+        change["text"] = change["text"] + event.char
+window.bind("<Key>", keypress)
+
+window.mainloop()
